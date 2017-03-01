e673f6e1c9ca5798ae0a33744401ed241ee2433f
VrsajkovIvan33 <vrsajkov.ivan@gmail.com>
2017-03-01 06:21:57 +0100

    Update import.sql

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,3 +1,3 @@
-INSERT INTO restaurantuser(uname, surname, password, email, utype, version) VALUES ("Boris", "Janjic", "boki", "cmd3395@yahoo.com", "GUEST", 0);
-INSERT INTO guest(active, id) VALUES (true , (SELECT id FROM user WHERE email="cmd3395@yahoo.com" AND password="boki"));
+INSERT INTO restaurantuser (uname, surname, password, email, utype, version) VALUES ('Boris', 'Janjic', 'boki', 'cmd3395@yahoo.com', 'GUEST', 0);
+INSERT INTO guest(active, id) VALUES (true , (SELECT id FROM user WHERE email='cmd3395@yahoo.com' AND password='boki'));
 
